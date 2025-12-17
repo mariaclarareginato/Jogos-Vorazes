8f8841a9fa613b442d007a1bcf6622da784b4d25
Maria Clara Reginato <mariaclarareginato8@gmail.com>
2025-12-16 21:00:35 -0300

    Atualização Layout e Next

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
